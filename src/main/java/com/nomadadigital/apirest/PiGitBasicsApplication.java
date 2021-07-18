a85de4bbec579d17139729821376ed25e6fa2b65
Lucye <lethiciass.r@gmail.com>
2021-07-17 23:15:31 -0500

    Inicio del proyecto

---
+++ src/main/java/com/nomadadigital/apirest/PiGitBasicsApplication.java
@@ -0,0 +1,13 @@
+package com.nomadadigital.apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiGitBasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiGitBasicsApplication.class, args);
+	}
+
+}
