ee50fa4710514fe570ee1539f3d9844285e09b6b
Lucye <lethiciass.r@gmail.com>
2021-07-18 09:36:04 -0500

    Endpoint nuevo

--- src/main/java/com/nomadadigital/apirest/controller/GitController.java
+++ src/main/java/com/nomadadigital/apirest/controller/GitController.java
@@ -1,14 +1,21 @@
 package com.nomadadigital.apirest.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class GitController {
 	
 	@GetMapping("/test")
 	public String test() {
 		return "Test";
+	}
+	
+	@GetMapping("/greet")
+	public String greet() {
+		return "Hello everyone";
 	}
 
 }
