465e32d6fbe36e3dbe6d64dd913ee3d5f760a1d7
Lucye <lethiciass.r@gmail.com>
2021-07-18 09:45:41 -0500

    Post method agregado a la Api

--- src/main/java/com/nomadadigital/apirest/controller/GitController.java
+++ src/main/java/com/nomadadigital/apirest/controller/GitController.java
@@ -1,6 +1,8 @@
 package com.nomadadigital.apirest.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +18,11 @@ public class GitController {
 	@GetMapping("/greet")
 	public String greet() {
 		return "Hello everyone";
+	}
+	
+	@PostMapping("/save")
+	public String save(@RequestBody String value) {
+		return value;
 	}
 
 }
